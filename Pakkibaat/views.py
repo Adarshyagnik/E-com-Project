054bae6ee865d5ce660e1e811ca2d090db8b55d8
Adarsh <yagnikaadarsh5@gmail.com>
2022-12-15 00:33:56 +0530

    firstCommit

---
+++ Pakkibaat/views.py
@@ -0,0 +1,8 @@
+
+#from django.shortcuts import 
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'index.html')
